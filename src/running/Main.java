136bf416a2cf30dfac534ad9ac2efad2055ce7a6
sadjad <sghgh1996@yahoo.com>
2017-05-15 12:01:41 +0430

    initializing project

---
+++ src/running/Main.java
@@ -0,0 +1,16 @@
+package running;
+
+/**
+ * Created by sadjad-pc on 5/15/17.
+ *
+ */
+public class Main {
+
+    public Main(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("running");
+    }
+}
