779c6b940678663328378e0d916606b0b299a3dc
sadjad <sghgh1996@yahoo.com>
2017-05-20 11:37:07 +0430

    Project framework added from mmsamiee

---
+++ src/Problems/Problem.java
@@ -0,0 +1,14 @@
+package Problems;
+import java.util.Vector;
+
+public abstract class Problem<State,Action>{
+    public abstract State initiallState();
+    public abstract Vector<Action> actions(State state);
+    public abstract State result(State state,Action action);
+    public abstract boolean goal(State state);
+    public abstract void print(State state);
+    public abstract double objective_function(State state);
+    public abstract State randomState();
+    public abstract State crossOver(State x,State y);
+    public abstract State mutation(State x);
+}
---
+++ src/algorithmes/SimulatedAnnealing.java
@@ -0,0 +1,76 @@
+package algorithmes;
+
+import java.util.Vector;
+
+import Problems.Problem;
+
+public class SimulatedAnnealing<S, A> {
+    Problem<S, A> problem;
+
+    public SimulatedAnnealing(Problem<S, A> problem) {
+        super();
+        this.problem = problem;
+    }
+
+    public void search(int scheduleMethod) {
+        int visitedNodes = 0;
+        int expandedNodes = 0;
+        S currentState = problem.initiallState();
+        for (int t = 1; t <= 200; t++) {
+            double T = schedule(t, scheduleMethod);
+            if (T == 0) {
+                break;
+            }
+            if (problem.goal(currentState)) {
+                break;
+            }
+            S nextState;
+            Vector<A> actions = problem.actions(currentState);
+            expandedNodes++;
+            int random = (int) (Math.random() * actions.size());
+            if (actions.size() == 0)
+                break;
+            nextState = problem.result(currentState, actions.elementAt(random));
+            double deltaE = problem.objective_function(nextState)
+                    - problem.objective_function(currentState);
+            visitedNodes++;
+            if (deltaE > 0)
+                currentState = nextState;
+            else {
+                double possibility = Math.random();
+                if (possibility < Math.pow((Math.E), (deltaE / T))) {
+                    currentState = nextState;
+                }
+            }
+        }
+        print(visitedNodes, expandedNodes, currentState);
+    }
+
+    public double schedule(int t, int method) {
+        Double T;
+        switch (method) {
+            case 0:
+                T = (double) (100 * Math.pow(0.9, t));
+                return T;
+            case 1:
+                T = (double) (100 / (double) t);
+                return T;
+            case 2:
+                T = (double) (100 / (double)( t*t));
+                return T;
+            default:
+                T = (double) (100 / (double)( t*t));
+                return T;
+        }
+    }
+
+    public void print(int visitedNodes, int expandedNodes, S state) {
+        System.out.println("Number of Visited Nodes:\t" + visitedNodes);
+        System.out.println("Number of expanded Nodes:\t" + expandedNodes);
+        System.out.println("Solution:\n");
+        problem.print(state);
+        System.out.println("solution fitness is :\t"
+                + problem.objective_function(state));
+    }
+
+}
